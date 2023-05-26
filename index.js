b01adb6aac5a2afdb0d77fa618d690a0179a6362
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-27 01:30:35 +0600

    get notiece

--- index.js
+++ index.js
@@ -23,16 +23,16 @@ async function run() {
   try {
     await client.connect();
 
-    const userCollection = client.db("counselingManagement").collection("user");
+    const userCollection = client.db('counselingManagement').collection('user');
     const appointmentCollection = client
-      .db("counselingManagement")
-      .collection("appointments");
+      .db('counselingManagement')
+      .collection('appointments');
     const bookingCollection = client
-      .db("counselingManagement")
-      .collection("bookings");
+      .db('counselingManagement')
+      .collection('bookings');
     const noticeCollection = client
-      .db("counselingManagement")
-      .collection("notice");
+      .db('counselingManagement')
+      .collection('notice');
 
     // // // // // // // // // // // //
 
@@ -40,7 +40,7 @@ async function run() {
 
     // create and update User
     //create and update a user
-    app.put("/create-user/:email", async (req, res) => {
+    app.put('/create-user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
 
@@ -59,7 +59,7 @@ async function run() {
       res.send(result);
     });
     // get all users from db
-    app.get("/users", async (req, res) => {
+    app.get('/users', async (req, res) => {
       const query = {};
 
       const cursor = userCollection.find(query);
@@ -69,7 +69,7 @@ async function run() {
     });
 
     // // all User filter by email category
-    app.get("/user/:email", async (req, res) => {
+    app.get('/user/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const cursor = userCollection.find(query);
@@ -81,7 +81,7 @@ async function run() {
 
     // // get appointments to query multiple collection  and them marge data
 
-    app.get("/appointments", async (req, res) => {
+    app.get('/appointments', async (req, res) => {
       const date = req.query.date;
       const query = {};
       const options = await appointmentCollection.find(query).toArray();
@@ -90,13 +90,13 @@ async function run() {
         .find(bookingQuery)
         .toArray();
       //
-      options.forEach((option) => {
+      options.forEach(option => {
         const optionBooked = alreadyBooked.filter(
-          (book) => book.teacherName === option.name
+          book => book.teacherName === option.name
         );
-        const bookedSlots = optionBooked.map((book) => book.slot);
+        const bookedSlots = optionBooked.map(book => book.slot);
         const remainingSlots = option.slots.filter(
-          (slot) => !bookedSlots.includes(slot)
+          slot => !bookedSlots.includes(slot)
         );
         option.slots = remainingSlots;
       });
@@ -104,20 +104,20 @@ async function run() {
     });
 
     // Post appointments
-    app.post("/appointments", async (req, res) => {
+    app.post('/appointments', async (req, res) => {
       const appointmentsBook = req.body;
       const result = await appointmentCollection.insertOne(appointmentsBook);
       res.send(result);
     });
 
     // post Booking/counseling
-    app.post("/bookings", async (req, res) => {
+    app.post('/bookings', async (req, res) => {
       const newBooking = req.body;
       const result = await bookingCollection.insertOne(newBooking);
       res.send(result);
     });
     // get Booking/counseling
-    app.get("/bookings", async (req, res) => {
+    app.get('/bookings', async (req, res) => {
       const query = {};
       const cursor = bookingCollection.find(query);
       const result = await cursor.toArray();
@@ -125,14 +125,14 @@ async function run() {
     });
 
     // //  Booking/counseling filter by Teacher email
-    app.get("/bookings/:email", async (req, res) => {
+    app.get('/bookings/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const cursor = bookingCollection.find(query);
       const user = await cursor.toArray();
       res.send(user);
     });
-    app.get("/booking/:email", async (req, res) => {
+    app.get('/booking/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const cursor = bookingCollection.find(query);
@@ -141,14 +141,14 @@ async function run() {
     });
 
     //  Booking/counseling filter by Student email
-    app.get("/studentBooking/:studentsEmail", async (req, res) => {
+    app.get('/studentBooking/:studentsEmail', async (req, res) => {
       const studentsEmail = req.params.studentsEmail;
       const query = { studentsEmail };
       const cursor = bookingCollection.find(query);
       const user = await cursor.toArray();
       res.send(user);
     });
-    app.get("/studentBookings/:studentsEmail", async (req, res) => {
+    app.get('/studentBookings/:studentsEmail', async (req, res) => {
       const studentsEmail = req.params.studentsEmail;
       const query = { studentsEmail };
       const cursor = bookingCollection.find(query);
@@ -157,7 +157,7 @@ async function run() {
     });
 
     // // Delete one Booking counseling
-    app.delete("/bookings/:id", async (req, res) => {
+    app.delete('/bookings/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
@@ -165,12 +165,20 @@ async function run() {
     });
 
     //                    Notice              //
-// Post Notice
-  app.post('/notices', async (req, res) => {
-    const postResult = req.body;
-    const result = await noticeCollection.insertOne(postResult);
-    res.send(result);
-  });
+    // Post Notice
+    app.post('/notices', async (req, res) => {
+      const postResult = req.body;
+      const result = await noticeCollection.insertOne(postResult);
+      res.send(result);
+    });
+
+    // Get all Notice
+    app.get('/notices', async (req, res) => {
+      const query = {};
+      const cursor = noticeCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
